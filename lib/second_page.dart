0b89ce116ada72fb746df73d67ef3ab34fac4e37
Sigma <fikri.ptik@gmail.com>
2021-10-21 13:40:11 +0700

    multi-page flutter

---
+++ lib/second_page.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class SecondPage extends StatelessWidget {
+  const SecondPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("SECOND PAGE"),
+      ),
+      body: Center(
+        child: RaisedButton(
+          child: Text("BACK"),
+          onPressed: () {
+            Navigator.pop(context);
+          },
+        ),
+      ),
+    );
+  }
+}
